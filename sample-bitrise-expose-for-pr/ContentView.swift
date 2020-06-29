17f39fbfcbff6e39b22d1679484970542b4c7673
sato-shin <shin.sato@asken.inc>
2020-06-29 18:48:47 +0900

    Add xcode project

---
+++ sample-bitrise-expose-for-pr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample-bitrise-expose-for-pr
+//
+//  Created by sato-shin on 2020/06/29.
+//  Copyright © 2020 satoshin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
